5cc019e391ace71bcd3570c93e2a92c336cd02a3
zam-dev <zamsyh.work@gmail.com>
2022-05-09 18:37:35 +0700

    Setup tall stack

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @hasSection('title')
+
+            <title>@yield('title')</title>
+        @else
+            <title>{{ config('app.name') }}</title>
+        @endif
+
+        <!-- Favicon -->
+		<link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ url(mix('css/app.css')) }}">
+        @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ url(mix('js/app.js')) }}" defer></script>
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    </head>
+
+    <body>
+        @yield('body')
+
+        @livewireScripts
+        <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js" data-turbolinks-eval="false" data-turbo-eval="false"></script>
+    </body>
+</html>
